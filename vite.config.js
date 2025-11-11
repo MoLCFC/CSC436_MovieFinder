8e59cefa3af046341ae8f77a3eb5890bbe6952b1
Mahmood G <mwgheith@gmail.com>
2025-11-10 23:41:35 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Set base for GitHub Pages repo deployment
+  // Replace with your repo name if different
+  base: '/CSC436_MovieFinder/',
   plugins: [react()],
 })
